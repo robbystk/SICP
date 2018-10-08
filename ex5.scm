28ddaf0462e0ba7a8cf9cf1af1d2807b93de1c2b
Robby Stokoe <robbystk@gmail.com>
2018-10-07 20:26:58 -0600

    Procedure to reverse a list

--- ex5.scm
+++ ex5.scm
@@ -8,3 +8,10 @@
         (car l)
         (last-pair (cdr l))))
 
+; 1.2 reverse
+(define (reverse l)
+    (define (reverse-iter l acc)
+        (if (null? (cdr l))
+            (append (list (car l)) acc)
+            (reverse-iter (cdr l) (append (list (car l)) acc))))
+    (reverse-iter l (list)))
